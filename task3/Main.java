9a110e9eee281965007c5b82db207f50818efd19
kapishg10 <62251249+kapishg10@users.noreply.github.com>
2021-04-21 22:50:54 +0530

    Task 3

--- task3/Main.java
+++ task3/Main.java
@@ -7,18 +7,18 @@ class Main {
 		
 		TeacherDaoImpl teacherDao = new TeacherDaoImpl();
 
-	      //print all students
+	      //print all teachers
 	      for (Teacher student : teacherDao.getAllTeachers()) {
 	         System.out.println("Teacher: [Id : " + student.getTeacherId() + ", Name : " + student.getTeacherName() + " ]");
 	      }
 
 
-	      //update student
+	      //update teacher
 	      Teacher teacher = teacherDao.getAllTeachers().get(0);
 	      teacher.setTeacherName("Manpreet");
 	      teacherDao.updateTeacher(teacher);
 
-	      //get the student
+	      //get the teacher
 	      teacherDao.getTeacher(0);
 	      System.out.println("Teacher: [Id : " + teacher.getTeacherId() + ", Name : " + teacher.getTeacherName() + " ]");	
 	}
